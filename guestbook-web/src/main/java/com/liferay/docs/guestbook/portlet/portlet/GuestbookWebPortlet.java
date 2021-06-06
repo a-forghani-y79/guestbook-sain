178fb28dd22413a6c2899c3dad2c5cb2dee448a2
ali forghani <a.forghani.y79@gmail.com>
2021-06-07 00:52:51 +0430

    add entry action completed

--- guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
+++ guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
@@ -1,13 +1,21 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
+import com.liferay.docs.guestbook.model.GuestBookEntry;
 import com.liferay.docs.guestbook.portlet.constants.GuestbookWebPortletKeys;
 
 import com.liferay.docs.guestbook.service.GuestBookEntryLocalService;
 import com.liferay.docs.guestbook.service.GuestBookLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -36,6 +44,44 @@ public class GuestbookWebPortlet extends MVCPortlet {
     @Reference
     private GuestBookLocalService guestBookLocalService;
     @Reference
-    private GuestBookEntryLocalService guestBookLocalEntryService;
+    private GuestBookEntryLocalService guestBookEntryLocalService;
+
+    public void addEntry(ActionRequest request, ActionResponse response) throws PortalException {
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(GuestBookEntry.class.getName(),request);
+
+        //fetch data from request
+        String name = ParamUtil.getString(request,"name");
+        String email = ParamUtil.getString(request,"email");
+        String message = ParamUtil.getString(request,"message");
+        long entryId = ParamUtil.getLong(request,"entryId");
+        long guestbookId = ParamUtil.getLong(request,"guestbookId");
+
+
+        if (entryId>0){
+            //that means we should update entry
+            // shroud with try catch for if got exception, copy request params into response and render edit_entry.jsp
+            try {
+                guestBookEntryLocalService.updateGuestbookEntry(serviceContext.getUserId(), guestbookId, entryId, name, email, message, serviceContext);
+
+                //find a replace for this
+                response.setRenderParameter("guestbookId", Long.toString(guestbookId));
+            }catch (Exception e){
+                System.out.println(e);
+                PortalUtil.copyRequestParameters(request,response);
+                response.setRenderParameter("mcvPath", "/guestbook/edit_entry.jsp");
+            }
+
+        }else {
+            //that means we should create new entry
+            try{
+                guestBookEntryLocalService.addGuestbookEntry(serviceContext.getUserId(),guestbookId,name,email,message,serviceContext);
+                response.setRenderParameter("guestbookId",Long.toString(guestbookId));
+            }catch (Exception e){
+                System.out.println(e);
+                PortalUtil.copyRequestParameters(request,response);
+                response.setRenderParameter("mvcPath","/guestbook/edit_entry.jsp");
+            }
+        }
+    }
 
 }
