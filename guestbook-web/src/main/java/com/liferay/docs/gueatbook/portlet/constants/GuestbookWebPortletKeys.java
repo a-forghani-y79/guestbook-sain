f401fe6cc4cad95460813185b9b645e716ab42de
ali forghani <a.forghani.y79@gmail.com>
2021-06-06 21:13:32 +0430

    guestbook-web init

---
+++ guestbook-web/src/main/java/com/liferay/docs/gueatbook/portlet/constants/GuestbookWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.gueatbook.portlet.constants;
+
+/**
+ * @author jinos
+ */
+public class GuestbookWebPortletKeys {
+
+	public static final String GUESTBOOKWEB =
+		"com_liferay_docs_gueatbook_portlet_GuestbookWebPortlet";
+
+}
